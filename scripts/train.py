1cf1b74dca0891d78e7bda8740d65aecba8a8e72
davidh-lambda <142889449+davidh-lambda@users.noreply.github.com>
2024-10-11 23:00:55 +0200

    config: wandb project name -> sora_speedrun

--- scripts/train.py
+++ scripts/train.py
@@ -79,7 +79,7 @@ def main():
     if coordinator.is_master():
         tb_writer = create_tensorboard_writer(exp_dir)
         if cfg.get("wandb", False):
-            wandb.init(project="Open-Sora", name=exp_name, config=cfg.to_dict(), dir="./outputs/wandb")
+            wandb.init(project="sora_speedrun", name=exp_name, config=cfg.to_dict(), dir="./outputs/wandb")
 
     # == init ColossalAI booster ==
     plugin = create_colossalai_plugin(
